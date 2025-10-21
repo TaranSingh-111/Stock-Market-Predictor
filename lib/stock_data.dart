560fda3e0f205e439e63a9ef7ce07e66da89b0c6
TaranSingh-111 <taransingh761111@gmail.com>
2025-10-21 15:06:47 +0530

    added stock details

---
+++ lib/stock_data.dart
@@ -0,0 +1,16 @@
+class StockData{
+  final double open;
+  final double high;
+  final double low;
+  final double marketCap;
+  final double peRatio;
+  final double high52;
+  final double low52;
+  final double dividend;
+  final double qtrDivAmt;
+
+  StockData(
+      required this.open,
+      req
+      )
+}
